91df8e15036e79e5cb4c7932f982fcc3dffdf9cd
abhishek04012005 <abhishekchoudhary5414@gmail.com>
2024-12-25 02:09:26 +0530

    seosnhdd

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import HelmetExport from 'react-helmet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <HashRouter> */}
+    {/* <HelmetExport> */}
       <App />
-    {/* </HashRouter> */}
+    {/* </HelmetExport> */}
   </React.StrictMode>
 );
 
